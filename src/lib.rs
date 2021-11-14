5bd6113ce5f81b35bf1b765f52240fe93eb8e8b3
Rick <richardhob@gmail.com>
2021-11-14 10:35:04 -0800

    add regex and lazy static stuff, start working in base implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,13 @@
 
-pub fn parse_string(line: &str) -> i32 {
-    0
+use lazy_static::lazy_static;
+use regex::Regex;
+
+pub fn parse_string(line: &str) -> Result<i32, &'static str> {
+    lazy_static! {
+        static ref RE: Regex = Regex::new(r".*").unwrap();
+    }
+    if RE.is_match(line) { return Ok(0); }
+    else { return Err("No match"); }
 }
 
 #[cfg(test)]
@@ -9,6 +16,13 @@ mod tests {
 
     #[test]
     fn parse_prototype() {
+        let result = parse_string("int test(void);").unwrap();
+        assert_eq!(result, 0);
+    }
+
+    #[test]
+    fn parse_result_ok() {
         let result = parse_string("int test(void);");
+        assert!(result.is_ok());
     }
 }
