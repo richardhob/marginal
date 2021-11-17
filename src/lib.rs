ac13c7702747abebd456a917ea61ae0098fb5f84
Rick <richardhob@gmail.com>
2021-11-17 10:38:37 -0800

    Add Regex, which works on some level :)

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,28 @@
 use lazy_static::lazy_static;
 use regex::Regex;
 
-pub fn parse_string(line: &str) -> Result<i32, &'static str> {
+pub struct FunctionPrototype <'a> {
+    return_type: &'a str,
+    name: &'a str,
+}
+
+pub fn parse_string(line: &str) -> Result<FunctionPrototype, &'static str> {
     lazy_static! {
-        static ref RE: Regex = Regex::new(r".*").unwrap();
+        static ref RE: Regex = Regex::new(r"^([\w\d]+) ([\w\d]+) ?\((.+)\);").unwrap();
     }
-    if RE.is_match(line) { return Ok(0); }
+    
+    if RE.is_match(line) { 
+        let captures = RE.captures(line).unwrap();
+
+        let rt = captures.get(1).map_or("", |m| m.as_str());
+        let name = captures.get(2).map_or("", |m| m.as_str());
+
+        let result = FunctionPrototype { 
+            return_type:rt,
+            name:name,
+        };
+        return Ok(result); 
+    }
     else { return Err("No match"); }
 }
 
@@ -15,9 +32,19 @@ mod tests {
     use super::*;
 
     #[test]
-    fn parse_prototype() {
+    fn parse_prototype_something() {
+        let result = parse_string("void something(void);").unwrap();
+
+        assert_eq!(result.return_type, "void");
+        assert_eq!(result.name, "something");
+    }
+
+    #[test]
+    fn parse_prototype_test() {
         let result = parse_string("int test(void);").unwrap();
-        assert_eq!(result, 0);
+
+        assert_eq!(result.return_type, "int");
+        assert_eq!(result.name, "test");
     }
 
     #[test]
